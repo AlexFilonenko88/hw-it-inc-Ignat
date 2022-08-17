eb5bb302395e2714f9e14f8b40fe1be291f3d51c
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-08-17 17:42:44 +0300

    add task one

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import s from './Message.module.css'
+
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessageType) {
+    return (
+        <div className={s.message}>
+            <img src={props.avatar} className={s.messageImg} alt={"Avatar"}/>
+            <div className={s.wrapper}>
+                <p className={s.name}>{props.name}</p>
+                <span className={s.messageM}>{props.message}</span>
+                <span className={s.time}>{props.time}</span>
+            </div>
+        </div>
+    )
+}
+
+export default Message
