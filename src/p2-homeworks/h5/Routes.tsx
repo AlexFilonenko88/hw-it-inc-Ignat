aa847924108128bedd97a88984fe88bf5d9ed527
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-10-07 17:10:28 +0300

    fix 5 hw

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import {Switch} from 'react-router-dom';
-import {Redirect, Route} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import PreJunior from "./pages/PreJunior";
 import Error404 from "../h5-rrd-v6/pages/Error404";
 import JuniorPlus from "./pages/JuniorPlus";
@@ -16,27 +15,27 @@ function Junior() {
     return null;
 }
 
-function Routes() {
+function Nav() {
     return (
         <div>
             {/*Switch выбирает первый подходящий роут*/}
-            <Switch>
+            <Routes>
 
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
                 {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-                <Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
-                <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
-                <Route path={PATH.JUNIOR} render={() => <Junior/>}/>
-                <Route path={PATH.JUNIOR_PLUS} render={() => <JuniorPlus/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
                 // add routes
 
                 {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                <Route render={() => <Error404/>}/>
+                <Route element={<Error404/>}/>
 
-            </Switch>
+            </Routes>
         </div>
     )
 }
 
-export default Routes
+export default Nav
