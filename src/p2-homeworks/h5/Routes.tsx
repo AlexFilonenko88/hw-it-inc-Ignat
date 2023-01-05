ded1a5c3ee3d617bc3c79e37473dcb5bb383b264
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2023-01-05 17:39:01 +0300

    10 hw fix errors

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -5,37 +5,37 @@ import Error404 from "../h5-rrd-v6/pages/Error404";
 import JuniorPlus from "./pages/JuniorPlus";
 
 export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    // add paths
-    JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
+  PRE_JUNIOR: '/pre-junior',
+  // add paths
+  JUNIOR: '/junior',
+  JUNIOR_PLUS: '/juniorPlus',
 }
 
 function Junior() {
-    return null;
+  return null;
 }
 
 function Nav() {
-    return (
-        <div>
-            {/*Switch выбирает первый подходящий роут*/}
-            <Routes>
+  return (
+    <div>
+      {/*Switch выбирает первый подходящий роут*/}
+      <Routes>
 
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-                {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+        {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
+        {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
+        <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
-                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
-                <Route path={PATH.JUNIOR} element={<Junior/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-                // add routes
+        <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+        <Route path={PATH.JUNIOR} element={<Junior/>}/>
+        <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+        // add routes
 
-                {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                <Route element={<Error404/>}/>
+        {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+        <Route element={<Error404/>}/>
 
-            </Routes>
-        </div>
-    )
+      </Routes>
+    </div>
+  )
 }
 
 export default Nav
